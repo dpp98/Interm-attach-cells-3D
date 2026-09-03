4aa0e036b7885b35b5fb64f290f08b96fb7f8116
dpp98 <dppanigrahi98@gmail.com>
2026-09-04 04:23:22 +0530

    Update compute_emg_diff.py

--- Post_process/compute_emg_diff.py
+++ Post_process/compute_emg_diff.py
@@ -8,12 +8,12 @@ Created on Mon Apr 28 09:23:51 2025
 
 import math
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 filename = "Dt.xyz"
 filehandle = open(filename,'r')
 
-nlines = 9999
+nlines = 7000
 oosix = 1.0/6.0
 
 time = np.zeros(nlines,dtype=float)
@@ -25,18 +25,18 @@ for i in range(nlines):
     
     split = s.split(" ")
     
-    time[i] = float(split[0])*(100/3600)
-    msd[i] = float(split[1])*(100/1000000)
+    time[i] = float(split[0])
+    msd[i] = float(split[1])
 
 
-nskip = 5000       # Skip initial few frames to ensure that the measurement is taken at steady state
-dt = 100.0/3600.0
+nskip = 3000
+dt = 100.0 # in s
 deff = np.zeros(nlines-nskip,dtype=float)
 stride = 500
 
 for i in range(nlines-nskip):
     
-    deff[i] = (1000000/60) * oosix * (msd[nskip+i]-msd[nskip+i-stride]) / (stride*dt) # micron^2/min
+    deff[i] = 100 * oosix * (msd[nskip+i]-msd[nskip+i-stride]) / (stride*dt) # micron^2/s
 
 
 print(np.mean(deff),np.std(deff))
